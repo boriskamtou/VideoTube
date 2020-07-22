2c1468cc0ee90ce61830b5c7e6d9826e73513f1a
kamtou boris <boriskamtou@gmail.com>
2020-07-22 15:42:21 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>VideoTube</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div id="pageContainer">
+        <div id="maskHeadContainer">
+        </div>
+        <div id="sideNavContainer"></div>
+        <div id="mainSectionContainer"></div>
+    </div>
+</body>
+
+</html>
